1fd93b891cf000d341f2bbb7366fe2425f365797
Jason Galea <jasonpgalea@outlook.com>
2022-02-08 21:57:19 +1100

    Refactored. Deploy & destroy working. DNS next

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Deploy CTs
-terraform -chdir=terraform/ init
-terraform -chdir=terraform/ plan
-terraform -chdir=terraform/ apply -auto-approve
-
-# Configure CTs
-ansible-playbook -i ansible/inv.yml ansible/configure.yml
