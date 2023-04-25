d083e49ea579b0599183e391298d8168f22536fb
JG <jasongalea98@outlook.com>
2023-04-25 14:42:19 +1000

    Added some fun extras to prox initial config

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 echo -e "\n==> Updating package list"
 sudo apt update -y
 
-echo -e "\n==> Installing 'sshpass'"
+echo -e "\n==> Install 'sshpass'"
 sudo apt install -y sshpass
 
-echo -e "\n==> Upgrading pip"
+echo -e "\n==> Upgrade pip"
 python3 -m pip install -U pip setuptools wheel
 
-echo -e "\n==> Installing pip requirements"
+echo -e "\n==> Install pip requirements"
 python3 -m pip install -r reqs.txt
 
 echo -e "\n==> Show ansible version"
