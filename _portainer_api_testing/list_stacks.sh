baa24fe46abe4e9be6185085177105c8ba3231cf
JG <jasongalea98@outlook.com>
2023-05-21 00:44:35 +1000

    holy shit portainer api work now

--- _portainer_api_testing/list_stacks.sh
+++ _portainer_api_testing/list_stacks.sh
@@ -5,7 +5,7 @@ export API_URL="http://10.1.1.243:9000/api"
 export PORTAINER_PASSWORD=$(cat $BASE_DIR/../portainer_password.txt)
 export JWT_TOKEN=$(http POST $API_URL/auth Username="admin" Password="$PORTAINER_PASSWORD" | jq -r ."jwt")
 
-echo -e "==> Listing all stacks"
-http GET \
-      $API_URL/stacks \
+
+echo -e "\n==> List all stacks"
+http GET $API_URL/stacks \
       "Authorization: Bearer $JWT_TOKEN"
